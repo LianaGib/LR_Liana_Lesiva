71332defee696d8a2c36e22309f98002e4530eac
LianaGib <143552368+LianaGib@users.noreply.github.com>
2023-11-18 22:19:40 +0300

    Add files via upload

---
+++ Lab/main7.py
@@ -0,0 +1,14 @@
+# TODO Напишите функцию для поиска индекса товара
+def find_item_funk(produkt_list, search_product):
+    if search_product in produkt_list:
+        return produkt_list.index(search_product)
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_item_funk(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
